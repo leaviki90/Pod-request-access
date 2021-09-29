3276d43edbfbe584767bb6f14fbcd189350172f7
leaviki90 <leaviki90@gmail.com>
2021-09-29 21:17:45 +0200

    small changes

--- main.js
+++ main.js
@@ -1,47 +1,32 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 
-
-
-
-
 function showError(email, message) {
-const formControl = email.parentElement;
-formControl.className ="form-control error";
-const small = formControl.querySelector("small");
-small.innerText = message;
+  const formControl = email.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
-
-function showSuccess(email){
-const formControl = email.parentElement;
-formControl.className = "form-control success";
+function showSuccess(email) {
+  const formControl = email.parentElement;
+  formControl.className = "form-control success";
 }
 
-
-function isValidEmail(email){
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-
-
-
-
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-
-
-
-if(email.value.trim() === ""){
+  if (email.value.trim() === "") {
     showError(email, "Oops! Please add your email");
-    }else if(!isValidEmail(email.value)){
-        showError(email, "Oops! Please check your email");
-    }
-    
-    else {
-        showSuccess(email)
-    }
-
-})
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Oops! Please check your email");
+  } else {
+    showSuccess(email);
+  }
+});
